08ad21858a4cfe26ae1cc366fc9887b1740e6081
VinhT3rB0 <vinhvinh132465@gmail.com>
2024-07-21 14:35:47 +0700

    card

--- FE/src/page/Home/Home.jsx
+++ FE/src/page/Home/Home.jsx
@@ -7,6 +7,12 @@ import CardPhone from '../../components/ProductCardPhone/ProductCardPhone';
 import Banner from '../../components/Banner/Banner'
 import ContactBox from '../../components/ContactBox/ContactBox'
 import Laptop from '../../page/Laptop/Laptop'
+import Phone from '../Phone/Phone'
+import Pk from '../Pk/Pk'
+import LaptopHome from '../../components/Laptophome/LaptopHome'
+import PhoneHome from '../../components/Phonehome/PhoneHome'
+import Pkhome from '../../components/Pkhome/Pkhome'
+import CatBanner from '../../components/CardBanner/CatBanner'
 
 
 export default function Home() {
@@ -198,32 +204,18 @@ export default function Home() {
     <div>
       <Header></Header>
       {/* <ContactBox></ContactBox> */}
-      <Carousels></Carousels>
       <div className="container">
         <div className="row">
+          <Carousels></Carousels>
           <Banner></Banner>
           <CatSlider></CatSlider>
-          <h1 style={{ textAlign: 'center' }}>Điện thoại ưu chuộng</h1>
-          {/* {phoneProducts.map((product, index) => (
-            <div key={index} className="col-md-3">
-              <CardPhone product={product} />
-            </div>
-          ))} */}
-          <Laptop></Laptop>
-          <h1 style={{ textAlign: 'center', marginTop: 50 }}>Máy tính nổi bật</h1>
-
-          {sampleProducts.map((product, index) => (
-            <div key={index} className="col-md-3">
-              <CardPhone product={product} />
-            </div>
-          ))}
+          <h1 style={{ textAlign: 'center', marginTop: 50 }}>Laptop ưa chuộng</h1>
+          <LaptopHome></LaptopHome>
+          <CatBanner></CatBanner>
+          <h1 style={{ textAlign: 'center', marginTop: 50 }}>Điện thoại bán chạy</h1>
+          <PhoneHome></PhoneHome>
           <h1 style={{ textAlign: 'center', marginTop: 50 }}>Phụ kiện điện tử</h1>
-
-          {accessoryProducts.map((product, index) => (
-            <div key={index} className="col-md-3">
-              <CardPhone product={product} />
-            </div>
-          ))}
+          <Pkhome></Pkhome>
         </div>
       </div>
       <Footer></Footer>
